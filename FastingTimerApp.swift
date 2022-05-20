a9b42f7c171515877b6cd92b508cfd1888c30749
kharabe <97969001+kharabe@users.noreply.github.com>
2022-05-21 00:04:11 +0530

    Add files via upload

---
+++ FastingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingTimerApp.swift
+//  FastingTimer
+//
+//  Created by Aakash Kharabe on 15/05/22.
+//
+
+import SwiftUI
+
+@main
+struct FastingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
